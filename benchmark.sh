89e0c8c5683b822a9c9cf7fd6a73362cb77b48c4
Jeff Chen <chen.jeff@gmail.com>
2019-04-17 12:03:05 -0400

    add bench

---
+++ benchmark.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+for b in 32 64
+do
+    for d in 0 1 2 3
+    do
+        echo "Batch: "
+        echo $b
+        echo "Device: "
+        echo $d
+
+        time CUDA_VISIBLE_DEVICES=$d python3 main.py --epochs 5 --no-early-stop --batch-size $b
+
+    done
+done
